537b4964c71c4cfdb8d863b9629b7011adb2f396
Mina Long <minalong@Minas-Air.fios-router.home>
2020-10-15 20:40:29 -0400

    first commit

---
+++ src/main/java/com/foodtrackerbe/foodtrackerbe/FoodtrackerbeApplication.java
@@ -0,0 +1,13 @@
+package com.foodtrackerbe.foodtrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtrackerbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtrackerbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodtrackerbe/foodtrackerbe/FoodtrackerbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodtrackerbe.foodtrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtrackerbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
