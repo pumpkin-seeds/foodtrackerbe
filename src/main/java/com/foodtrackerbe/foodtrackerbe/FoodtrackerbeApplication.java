c719f2831ad03068b141b2fbbc9ec68b71ea6853
Mina Long <minalong@Minas-Air.fios-router.home>
2020-10-15 23:03:00 -0400

    tested jdbc connection worked

--- src/main/java/com/foodtrackerbe/foodtrackerbe/FoodtrackerbeApplication.java
+++ src/main/java/com/foodtrackerbe/foodtrackerbe/FoodtrackerbeApplication.java
@@ -1,13 +1,29 @@
 package com.foodtrackerbe.foodtrackerbe;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class FoodtrackerbeApplication {
+public class FoodtrackerbeApplication implements CommandLineRunner {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodtrackerbeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		runJdbc();
+	}
+
+	void runJdbc() {
+		jdbcTemplate.execute("CREATE TABLE dbo.FoodInfo2(FdcId int NULL)");
+		// jdbcTemplate.execute("DROP TABLE dbo.FoodInfo2");
 	}
 
 }
