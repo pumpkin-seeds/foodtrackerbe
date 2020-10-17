8ecebf1e25d2da154706c097e2a6ade4cc24407d
Mina Long <minalong@Minas-Air.fios-router.home>
2020-10-17 14:08:29 -0400

    implemented first DAO

---
+++ src/main/java/com/foodtrackerbe/foodtrackerbe/FoodInfoDao.java
@@ -0,0 +1,7 @@
+package com.foodtrackerbe.foodtrackerbe;
+
+public interface FoodInfoDao<T> {
+
+    int getCount(int id);
+
+}
---
+++ src/main/java/com/foodtrackerbe/foodtrackerbe/FoodService.java
@@ -0,0 +1,5 @@
+package com.foodtrackerbe.foodtrackerbe;
+
+public interface FoodService {
+
+}
