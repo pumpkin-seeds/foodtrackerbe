d8645eb888f965e5cd38661075c6cb9c0da0711b
Mina Long <minalong@Minas-Air.fios-router.home>
2020-10-20 21:09:53 -0400

    minor update on CORS

--- src/main/java/com/foodtrackerbe/foodtrackerbe/FoodController.java
+++ src/main/java/com/foodtrackerbe/foodtrackerbe/FoodController.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// To fix the CORS error, allow local FE to access local backend
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class FoodController {
 
